4f518a2de8412ce5c34926b41a07f81cebb8294f
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-03-07 10:08:03 -0600

    margarita ui

--- src/components/MargaritaCard.js
+++ src/components/MargaritaCard.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-function MargaritaCard({ margarita }) {
+function MargaritaCard({ margarita, addToFavorites }) {
   const { name, ingredients, directions, image, likes } = margarita;
 
   return (
     <div>
       <h2>{name}</h2>
       <img src={image} alt={name} />
+
       <h5>Recipie and directions</h5>
+      <button onClick={() => addToFavorites(margarita)}>
+        ❤️ Add To Favorites
+      </button>
     </div>
   );
 }
--- src/components/MargaritaList.js
+++ src/components/MargaritaList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import MargaritaCard from "./MargaritaCard";
 
-function MargaritaList({ margaritas }) {
+function MargaritaList({ margaritas, addToFavorites }) {
   //   console.log("in MargList: ", margaritas);
 
   return margaritas.map((margarita) => (
-    <MargaritaCard key={margarita.id} margarita={margarita} />
+    <MargaritaCard
+      key={margarita.id}
+      margarita={margarita}
+      addToFavorites={addToFavorites}
+    />
   ));
 }
 
