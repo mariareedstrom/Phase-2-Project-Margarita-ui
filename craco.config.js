caff9016b7d5b37e4b60bd70e26103682cb493c7
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-03-21 10:18:15 -0500

    for deployment/netlify

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
+// const Dotenv = require("dotenv-webpack");
+
 module.exports = {
   webpack: {
+    // plugins: [new Dotenv()],
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
     },
